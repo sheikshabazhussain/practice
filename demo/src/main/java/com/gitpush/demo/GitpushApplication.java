98a3abf793cf98b60fbcaa13909aa41aad201ea9
shabaz <sheikshabaz256@gmail.com>
2023-02-22 10:16:36 +0530

    spring

---
+++ demo/src/main/java/com/gitpush/demo/GitpushApplication.java
@@ -0,0 +1,13 @@
+package com.gitpush.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpushApplication.class, args);
+	}
+
+}
